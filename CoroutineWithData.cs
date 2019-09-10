a8d0e8ce1c33e16e3782936bd9d4fb7d4d37ad91
gngriffiths <gngriffiths@users.noreply.github.com>
2019-09-10 18:11:32 +0200

    Added CoroutineWithData for returning data from a coroutine

---
+++ CoroutineWithData.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Utility class to get data from a coroutine 
+// Instructions in comments at bottom of script
+// https://answers.unity.com/questions/24640/how-do-i-return-a-value-from-a-coroutine.html
+
+public class CoroutineWithData
+{
+    public Coroutine coroutine { get; private set; }
+    public object result;
+    private IEnumerator target;
+    public CoroutineWithData(MonoBehaviour owner, IEnumerator target)
+    {
+        this.target = target;
+        this.coroutine = owner.StartCoroutine(Run());
+    }
+
+    private IEnumerator Run()
+    {
+        while (target.MoveNext())
+        {
+            result = target.Current;
+            yield return result;
+        }
+    }
+}
+
+//// Use the below template to make a coroutine that returns data
+//private IEnumerator LoadSomeStuff()
+//{
+//    WWW www = new WWW("http://someurl");
+//    yield return www;
+//    if (String.IsNullOrEmpty(www.error) {
+//        yield return "success";
+//    }
+//    else
+//    {
+//        yield return "fail";
+//    }
+//}
+
+//// Invoke the coroutine LoadSomeStuff using the coroutine below and receive the data from LoadSomeStuff
+//IEnumerator GetLoadedData()
+//{
+//    coroutinewithdata cd = new coroutinewithdata(this, loadsomestuff());
+//    yield return cd.coroutine;
+//    debug.log("result is " + cd.result);  //  'success' or 'fail'
+//}
+
