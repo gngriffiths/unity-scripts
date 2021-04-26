0a22e2207055d7e78e9cc86bfd5dae5b1e1c4102
Gareth Griffiths <gareth.griffiths@uberact.com>
2021-04-26 15:18:17 +0100

    Added AgentDestinationPUN for Photon PUN and NavMeshAgent.

---
+++ AgentDestinationPUN.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.AI;
+
+// For silky smooth Photon PUN movement using NavMeshAgent
+
+public class AgentDestinationPUN : MonoBehaviourPun, IPunObservable
+{    
+    Vector3 agentDestination = new Vector3();
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            agentDestination = agent.destination;
+            stream.SendNext(agentDestination);
+        }
+        else
+        {
+            agentDestination = (Vector3)stream.ReceiveNext();
+        }
+    }
+
+    private void Update()
+    {
+        if (!this.photonView.IsMine)
+        {
+            agent.SetDestination(agentDestination);
+        }
+    }
+}
