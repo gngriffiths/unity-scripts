524e2f2648e0a6966205f9521adaf28def04e0fd
gngriffiths <g@untether.studio>
2023-07-13 14:37:51 +0100

    Create VContainer_InjectDynamically.cs

---
+++ VContainer_InjectDynamically.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class VContainer_InjectDynamically
+{
+    [Inject]
+    ClassToInject classToInject; 
+
+    // Inject this dynamically instantiated Monobehaviour.
+    void Construct()
+    {
+        var lifetimeScope = FindObjectOfType<LifetimeScope>();      // NTH Change from FindObjectOfType to something less compute intenstive.
+        var resolver = lifetimeScope.Container;
+        resolver.Inject(this);
+    }
+
+}
