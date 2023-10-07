d2566e50845d896cdecb01b152718a78883af530
gngriffiths <g@untether.studio>
2023-10-07 14:28:09 +0100

    Added simple game object pool

---
+++ GameObjectPool/GameObjectPool.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPool : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] int poolSize;
+
+    private Queue<GameObject> pool;
+
+    void Start()
+    {
+        // Initialize the object pool
+        pool = new Queue<GameObject>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(prefab, this.transform);
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+    }
+
+    // Use this function to get an object from the pool
+    public GameObject GetObjectFromPool()
+    {
+        if (pool.Count == 0)
+        {
+            // Automatica pool size increase: If the pool is empty, create a new object, add it to the pool, and return it the calling method. 
+            GameObject obj = Instantiate(prefab);
+            return obj;
+        }
+        else
+        {
+            // If the pool has objects, dequeue and return it the calling method.
+            GameObject obj = pool.Dequeue();
+            obj.SetActive(true);
+            return obj;
+        }
+    }
+
+    // Use this function to return an object to the pool
+    public void ReturnObjectToPool(GameObject obj)
+    {
+        obj.SetActive(false);
+        pool.Enqueue(obj);
+    }
+}
---
+++ GameObjectPool/GameObjectPoolManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+// Attach to a GameObject with a GameObjectPool component.
+
+public class GameObjectPoolManager : MonoBehaviour
+{
+    List<GameObject> activatedGameObjects;
+
+    GameObjectPool gameObjectPool
+    {
+        get
+        {
+            if (_gameObjectPool == null)
+                _gameObjectPool = GetComponentInChildren<GameObjectPool>();
+            return _gameObjectPool;
+        }
+    }
+    GameObjectPool _gameObjectPool;
+
+    void ReturnAllToPool()
+    {
+        foreach (var gameObject in activatedGameObjects)
+        {
+            gameObjectPool.ReturnObjectToPool(gameObject);
+        }
+        activatedGameObjects.Clear();
+    }
+
+    private void RemovePoint()
+    {
+        if (activatedGameObjects.Count == 0)
+            return;
+
+        gameObjectPool.ReturnObjectToPool(activatedGameObjects[0]);
+        activatedGameObjects.RemoveAt(0);
+    }
+}
+
