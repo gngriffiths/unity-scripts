f356ce9d8ea7358af02da95ce2db08a2b47098e1
gngriffiths <g@untether.studio>
2023-11-22 22:55:46 +0000

    GameObjectPool injects objects

---
+++ Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿//using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bootstrapper : MonoBehaviour
+{
+    async void Start()
+    {
+        Application.runInBackground = true;
+        //await UnityServices.InitializeAsync();
+
+        if (SceneManager.loadedSceneCount == 1)
+            SceneManager.LoadScene("CarSelection", LoadSceneMode.Additive);
+    }
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void Init()
+    {
+#if UNITY_EDITOR
+        var currentlyLoadedEditorScene = SceneManager.GetActiveScene();
+#endif
+
+        if (SceneManager.GetSceneByName("Bootstrapper").isLoaded != true)
+            SceneManager.LoadScene("Bootstrapper");
+
+#if UNITY_EDITOR
+        if (currentlyLoadedEditorScene.IsValid())
+            SceneManager.LoadSceneAsync(currentlyLoadedEditorScene.name, LoadSceneMode.Additive);
+#else
+        SceneManager.LoadSceneAsync("CarSelection", LoadSceneMode.Additive);
+#endif
+    }
+}
--- GameObjectPool/GameObjectPool.cs
+++ GameObjectPool/GameObjectPool.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using VContainer;
 
-public class GameObjectPool : MonoBehaviour
+public abstract class GameObjectPool : MonoBehaviour
 {
     [SerializeField] GameObject prefab;
     [SerializeField] int poolSize;
 
+    private IObjectResolver resolver;
+    private List<GameObject> activatedGameObjects = new List<GameObject>();
     private Queue<GameObject> pool;
+
+    [Inject]
+    public void Construct(IObjectResolver resolver)
+    {
+        this.resolver = resolver;
+    }
 
     void Awake()
     {
@@ -15,35 +24,74 @@ public class GameObjectPool : MonoBehaviour
 
         for (int i = 0; i < poolSize; i++)
         {
-            GameObject obj = Instantiate(prefab, this.transform);
-            obj.SetActive(false);
+            //GameObject obj = Instantiate(prefab, this.transform);
+            GameObject obj = CreateNewObject();
             pool.Enqueue(obj);
         }
+    }
+
+    GameObject CreateNewObject()
+    {
+        GameObject obj = Instantiate(prefab, this.transform);
+        obj.SetActive(false);
+        // Inject dependencies into each MonoBehaviour component of the GameObject.
+        foreach (var component in obj.GetComponents<MonoBehaviour>())
+        {
+            resolver.Inject(component);
+        }
+        return obj;
     }
 
     // Use this function to get an object from the pool
     public GameObject GetObjectFromPool()
     {
+        GameObject obj;
+
         if (pool.Count == 0)
         {
             // Automatica pool size increase: If the pool is empty, create a new object, add it to the pool, and return it the calling method. 
-            GameObject obj = Instantiate(prefab);
-            return obj;
+            obj = Instantiate(prefab);
         }
         else
         {
             // If the pool has objects, dequeue and return it the calling method.
-            GameObject obj = pool.Dequeue();
+            obj = pool.Dequeue();
             obj.SetActive(true);
-            return obj;
         }
+
+        activatedGameObjects.Add(obj);
+        return obj;
     }
 
     // Use this function to return an object to the pool
     public void ReturnObjectToPool(GameObject obj)
     {
+        bool go = activatedGameObjects.Contains(obj);
+        if (!go)
+            return;
+
         obj.transform.parent = transform;
         obj.SetActive(false);
         pool.Enqueue(obj);
+        activatedGameObjects.Remove(obj);
+    }
+
+    public void ReturnObjectsToPool(List<GameObject> objs)
+    {
+        foreach (var obj in objs)
+        {
+            ReturnObjectToPool(obj);
+        }
+    }
+
+    public void ReturnAllToPool()
+    {
+        foreach (var obj in activatedGameObjects)
+        {
+            obj.transform.parent = transform;
+            obj.SetActive(false);
+            pool.Enqueue(obj);
+        }
+        activatedGameObjects.Clear();
     }
 }
